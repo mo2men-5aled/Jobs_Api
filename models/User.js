aeb88b741b8e68d602c153fcf0f74f2d45938768
mo2men-5aled <momenk208@gmail.com>
2022-10-12 18:58:15 +0200

    generate a secret key using all keys generator + secret key to .env file + add the expiry tme from the .env file

--- models/User.js
+++ models/User.js
@@ -20,9 +20,13 @@ UserSchema.pre("save", async function () {
 });
 
 UserSchema.methods.createJWT = function () {
-  return jwt.sign({ userId: this._id, name: this.name }, "jwtsecret", {
-    expiresIn: "1h",
-  });
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
 };
 
 module.exports = mongoose.model("User", UserSchema);
