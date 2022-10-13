a5f820d6cdc92714dc87fba001a745a5e0668ae9
mo2men-5aled <momenk208@gmail.com>
2022-10-13 20:22:36 +0200

    update job

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,5 +1,6 @@
 const Job = require("../models/Job");
 
+//get all jobs
 const getAllJobs = async (req, res) => {
   const errorlog = [];
   const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
@@ -13,27 +14,7 @@ const getAllJobs = async (req, res) => {
   }
 };
 
-const getJob = async (req, res) => {
-  const errorlog = [];
-
-  if (req.params.id.length !== 24) {
-    errorlog.push(`no jobs found with id ${req.params.id}`);
-  } else {
-    const job = await Job.findOne({
-      createdBy: req.user.userId,
-      _id: req.params.id,
-    });
-
-    if (!job) {
-      errorlog.push(`no jobs found with id ${req.params.id}`);
-    } else {
-      res.status(200).json({ job });
-    }
-  }
-  if (errorlog.length > 0) {
-    res.status(404).json({ msg: errorlog });
-  }
-};
+//create a new job
 const createJob = async (req, res) => {
   const { company, position } = req.body;
   const errorlog = [];
@@ -70,8 +51,58 @@ const createJob = async (req, res) => {
     res.status(201).json({ job });
   }
 };
+
+//get a single job with the id
+const getJob = async (req, res) => {
+  const errorlog = [];
+
+  if (req.params.id.length !== 24) {
+    errorlog.push(`no jobs found with id ${req.params.id}`);
+  } else {
+    const job = await Job.findOne({
+      createdBy: req.user.userId,
+      _id: req.params.id,
+    });
+
+    if (!job) {
+      errorlog.push(`no jobs found with id ${req.params.id}`);
+    } else {
+      res.status(200).json({ job });
+    }
+  }
+  if (errorlog.length > 0) {
+    res.status(404).json({ msg: errorlog });
+  }
+};
+
+//update a job
 const updateJob = async (req, res) => {
-  res.json(req.user);
+  const errorlog = [];
+
+  const { company, position } = req.body;
+  if (!company) {
+    errorlog.push("company cannot be empty");
+  }
+
+  if (!position) {
+    errorlog.push("position cannot be empty");
+  }
+
+  if (company && position) {
+    const job = await Job.findOneAndUpdate(
+      { createdBy: req.user.userId, _id: req.params.id },
+      { company: company, position: position },
+      { new: true }
+    );
+    if (!job) {
+      errorlog.push(`no jobs found with id ${req.params.id}`);
+    } else {
+      res.status(200).json({ job });
+    }
+  }
+  if (errorlog.length > 0) {
+    res.status(400).json({ msg: errorlog });
+  }
 };
 const deleteJob = async (req, res) => {
   res.json(req.user);
