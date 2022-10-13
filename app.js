8ef88f968f451c160ece361eef22be4ab98e9f99
mo2men-5aled <momenk208@gmail.com>
2022-10-13 21:19:25 +0200

    add the security middlewares to the app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
+
+//extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 const authenticateUser = require("./middleware/authentication");
@@ -14,9 +21,20 @@ const jobsRouter = require("./routes/jobs");
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+const { RateLimiter } = require("rate-limiter");
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 minuits
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
