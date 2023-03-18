39949e25c7c4910adc3d402b7ee1362492346de8
mo2men-5aled <momenk208@gmail.com>
2023-03-18 15:48:58 +0200

    add the app url to CORS

--- app.js
+++ app.js
@@ -33,7 +33,11 @@ app.use(
 
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://main--shimmering-sunburst-377596.netlify.app/",
+  })
+);
 app.use(xss());
 
 // routes
